61b6b9c129c9c6d206450b9ef589ec5d9a49c6ef
lloydrhodescinch87 <lloyd.rhodes@cinch.co.uk>
2023-06-15 14:30:27 +0100

    add unit tests for command

---
+++ app/app/test.py
@@ -0,0 +1,24 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """test adding numbers together"""
+
+        res = calc.add(5, 6)
+
+        self.assertEquals(res, 11)
+
+    def test_subtract_numbers(self):
+        """Testing subtracting numbers"""
+
+        res = calc.subtract(8, 4)
+
+        self.assertEquals(res, 4)
