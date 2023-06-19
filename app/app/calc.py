cc9bcb6b5cc7f1a734807790ba581d6ff2851057
lloydrhodescinch87 <lloyd.rhodes@cinch.co.uk>
2023-06-19 11:57:54 +0100

    fix linting errors and db bug

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,7 +7,7 @@ def add(x, y):
     """Add x and y and return result"""
     return x + y
 
+
 def subtract(x, y):
     """Subtract y from x"""
     return x - y
-
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for db"""
 
@@ -19,13 +20,13 @@ class Command(BaseCommand):
 
         while db_up is False:
             try:
-                self.stdout.write(f"Getting here")
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                    self.stdout.write('Database unavailable, waiting 1 second...')
-           
-                    time.sleep(1)
+                self.stdout.write(Psycopg2Error)
+                self.stdout.write(
+                        'Database unavailable, waiting 1 second...'
+                    )
+                time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available'))
-
